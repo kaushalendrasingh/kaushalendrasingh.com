85d2529bc62de972b02bd13092a7f26e829620b8
Kaushal Singh <kaushalsingh@Kaushals-MacBook-Pro.local>
2025-09-26 16:47:45 +0530

    test

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://13.232.176.236:8000/',
+  baseURL: import.meta.env.VITE_API_URL || 'http://13.200.148.246:8000/',
 })
